2c98f0939f261cbc527d50d4fe453ff51f1c2a56
eevan7a9 <e7a9@protonmail.com>
2020-02-08 01:27:49 +0900

    added Board-header contents

--- frontend-trello/src/components/boards/BoardHeader.vue
+++ frontend-trello/src/components/boards/BoardHeader.vue
@@ -1,5 +1,27 @@
 <template>
-  <nav>header</nav>
+  <nav class="p-2">
+    <div class="board-title float-left font-weight-bold text-light ml-4 px-3">My Projects</div>
+    <div class="float-left text-light d-flex justify-content-center align-items-center mr-3">
+      <span class="board-header-btn p-2 d-flex justify-content-center align-items-center mr-2">
+        <img src="@/assets/icons/star.svg" alt="star" />
+      </span>|
+    </div>
+    <div class="float-left text-light d-flex justify-content-center align-items-center mr-3">
+      <span
+        class="board-header-btn p-2 d-flex justify-content-center align-items-center mr-2"
+      >Personal</span>|
+    </div>
+    <div class="float-left text-light d-flex justify-content-center align-items-center mr-3">
+      <span class="board-header-btn p-2 d-flex justify-content-center align-items-center mr-2">
+        <img src="@/assets/icons/globe.svg" alt="star" class="mr-2" /> Public
+      </span>|
+    </div>
+    <div class="float-right text-light d-flex justify-content-center align-items-center mr-3">
+      <span class="board-header-btn p-2 d-flex justify-content-center align-items-center mr-2">
+        <img src="@/assets/icons/more-horizontal.svg" alt="star" class="mr-2" /> Show Menu
+      </span>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -9,5 +31,25 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class BoardHeader extends Vue {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+nav {
+  .board-title {
+    background: transparent;
+    font-size: 18px;
+    line-height: 32px;
+    text-decoration: none;
+    max-width: calc(100% - 24px);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .board-header-btn {
+    background-color: rgba(0, 0, 0, 0.219);
+    border-radius: 5px;
+    img {
+      height: 20px;
+      width: 20px;
+    }
+  }
+}
 </style>
