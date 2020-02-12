d69e187383923cd1a967adabeb772b983c3529fb
eevan7a9 <e7a9@protonmail.com>
2020-02-12 20:47:01 +0900

    added content boardList popup

--- frontend-trello/src/components/boards/buttons/BoardList.vue
+++ frontend-trello/src/components/boards/buttons/BoardList.vue
@@ -21,7 +21,20 @@
       </header>
       <hr class="mt-0 pt-0" />
       <div class="px-4 text-dark w-100">
-        <p>boards...</p>
+        <div class="recent-boards">
+          <header class="text-left">
+            <img src="@/assets/icons/clock.svg" alt="clock" class="mr-1" />
+            <small class="font-weight-bold text-muted">RECENT BOADRS</small>
+          </header>
+          <div class="boards-list mt-2">
+            <div class="row board-item">
+              <div class="col-2 bg-dark py-2 img-container">---</div>
+              <div class="col-sm-9 text-left d-flex align-items-center label-container">
+                <h6 href="#" class="board-title text-dark font-weight-bold m-0">My Projects</h6>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -77,6 +90,33 @@ export default class BoardList extends Vue {
     background: hsla(0, 0%, 100%, 0.3);
     border-radius: 5px;
     border: 0px solid;
+  }
+  .recent-boards {
+    header {
+      img {
+        height: 20px;
+      }
+    }
+    .boards-list {
+      .board-item {
+        background: rgba(121, 121, 121, 0.349);
+        border-radius: 5px;
+        cursor: pointer;
+        &:hover {
+          background: rgba(121, 121, 121, 0.479);
+        }
+        .img-container {
+          // border-radius: 6px;
+          border-top-left-radius: 5px;
+          border-bottom-left-radius: 5px;
+        }
+        .label-container {
+          .board-title {
+            font-size: 15px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
