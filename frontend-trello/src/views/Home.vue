272020cf599567554316a70b486f8e3cdd03d4e8
eevan7a9 <e7a9@protonmail.com>
2020-02-13 05:59:04 +0900

    rename BoardView to BoardContent

--- frontend-trello/src/views/Home.vue
+++ frontend-trello/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home pt-5">
     <div class="main-board">
       <BoardHeader />
-      <BoardView class="mt-5" />
+      <BoardContent class="mt-5" />
     </div>
     <BoardMenubar class="sidebar-board" />
   </div>
@@ -10,14 +10,14 @@
 
 <script>
 // @ is an alias to /src
-import BoardView from "@/components/boards/BoardView.vue";
+import BoardContent from "@/components/boards/BoardContent.vue";
 import BoardHeader from "@/components/boards/BoardHeader.vue";
 import BoardMenubar from "@/components/boards/menubar/BoardMenubar.vue";
 export default {
   name: "Home",
   components: {
     BoardHeader,
-    BoardView,
+    BoardContent,
     BoardMenubar
   }
 };
