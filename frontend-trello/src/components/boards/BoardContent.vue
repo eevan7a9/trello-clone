7cd7058e7497df0907120397aadea4d799dc2d73
eevan7a9 <e7a9@protonmail.com>
2020-02-14 05:49:03 +0900

    added vuedraggable

--- frontend-trello/src/components/boards/BoardContent.vue
+++ frontend-trello/src/components/boards/BoardContent.vue
@@ -1,81 +1,112 @@
 <template>
   <div class="content-wrapper text-left">
     <div class="lists-container d-flex">
-      <div class="p-2 bg-light mx-2" id="listItem" v-for="list of lists" :key="list.id">
-        <h1>{{list.title}}</h1>
-      </div>
+      <draggable
+        class="list-group"
+        tag="div"
+        v-model="lists"
+        v-bind="dragOptions"
+        @start="isDragging = true"
+        @end="isDragging = false"
+      >
+        <transition-group type="transition" name="flip-list" class="d-flex">
+          <div
+            class="list-group-item p-2 bg-light mx-2"
+            id="listItem"
+            v-for="list of lists"
+            :key="list.id"
+          >
+            <h1>{{list.title}}</h1>
+          </div>
+        </transition-group>
+      </draggable>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-
-@Component({})
-export default class Board extends Vue {
-  lists = [
-    {
-      id: 1,
-      title: "List One",
-      cards: []
-    },
-    {
-      id: 2,
-      title: "List Two",
-      cards: []
-    },
-    {
-      id: 3,
-      title: "List Three",
-      cards: []
-    },
-    {
-      id: 4,
-      title: "List Four",
-      cards: []
-    },
-    {
-      id: 5,
-      title: "List Five",
-      cards: []
-    },
-    {
-      id: 6,
-      title: "List six",
-      cards: []
-    },
-    {
-      id: 7,
-      title: "List seven",
-      cards: []
-    },
-    {
-      id: 8,
-      title: "List eight",
-      cards: []
-    },
-    {
-      id: 9,
-      title: "List Nine",
-      cards: []
-    },
-    {
-      id: 10,
-      title: "List Ten",
-      cards: []
-    },
-    {
-      id: 11,
-      title: "List eleven",
-      cards: []
-    },
-    {
-      id: 12,
-      title: "List twelve",
-      cards: []
+<script>
+import draggable from "vuedraggable";
+export default {
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          title: "List One",
+          cards: []
+        },
+        {
+          id: 2,
+          title: "List Two",
+          cards: []
+        },
+        {
+          id: 3,
+          title: "List Three",
+          cards: []
+        },
+        {
+          id: 4,
+          title: "List Four",
+          cards: []
+        },
+        {
+          id: 5,
+          title: "List Five",
+          cards: []
+        },
+        {
+          id: 6,
+          title: "List six",
+          cards: []
+        },
+        {
+          id: 7,
+          title: "List seven",
+          cards: []
+        },
+        {
+          id: 8,
+          title: "List eight",
+          cards: []
+        },
+        {
+          id: 9,
+          title: "List Nine",
+          cards: []
+        },
+        {
+          id: 10,
+          title: "List Ten",
+          cards: []
+        },
+        {
+          id: 11,
+          title: "List eleven",
+          cards: []
+        },
+        {
+          id: 12,
+          title: "List twelve",
+          cards: []
+        }
+      ]
+    };
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
     }
-  ];
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -94,5 +125,25 @@ export default class Board extends Vue {
     display: inline-block;
     width: 400px;
   }
+}
+// dargdrop
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
 }
 </style>
