334bbbfd27b6b9a7af9ff861299dc1b91fb24e02
eevan7a9 <e7a9@protonmail.com>
2020-02-14 12:03:17 +0900

    added Board lists to separate component

--- frontend-trello/src/components/boards/BoardContent.vue
+++ frontend-trello/src/components/boards/BoardContent.vue
@@ -15,23 +15,7 @@
             v-for="list of lists"
             :key="list.id"
           >
-            <!-- hedear starts -->
-            <header class="d-flex justify-content-between align-items-center">
-              <h6 class="m-0 font-weight-bold py-2">{{list.title}}</h6>
-              <span>
-                <img src="@/assets/icons/more-horizontal-dark.svg" alt="star" class="mr-2" />
-              </span>
-            </header>
-            <!-- header ends -->
-            {{list.cards}}
-            <!-- footer starts -->
-            <footer class="add-card px-1 py-2">
-              <span>
-                <img src="@/assets/icons/plus-dark.svg" alt="star" class="mr-2" />
-              </span>
-              <span>Add another card</span>
-            </footer>
-            <!-- footer ends -->
+            <BoardList :list="list"></BoardList>
           </div>
         </transition-group>
       </draggable>
@@ -41,10 +25,11 @@
 
 <script>
 import draggable from "vuedraggable";
-
+import BoardList from "@/components/boards/BoardList";
 export default {
   components: {
-    draggable
+    draggable,
+    BoardList
   },
   computed: {
     lists: {
@@ -106,30 +91,5 @@ export default {
   max-height: 100%;
   position: relative;
   white-space: normal;
-  header {
-    cursor: pointer;
-    span {
-      img {
-        height: 15px;
-      }
-    }
-  }
-  .add-card {
-    cursor: pointer;
-    span {
-      color: grey;
-
-      img {
-        height: 18px;
-      }
-    }
-    &:hover {
-      background: #9e9fa344;
-      border-radius: 5px;
-      span {
-        color: #333;
-      }
-    }
-  }
 }
 </style>
---
+++ frontend-trello/src/components/boards/BoardList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- hedear starts -->
+    <header class="d-flex justify-content-between align-items-center">
+      <h6 class="m-0 font-weight-bold py-2">{{list.title}}</h6>
+      <span>
+        <img src="@/assets/icons/more-horizontal-dark.svg" alt="star" class="mr-2" />
+      </span>
+    </header>
+    <!-- header ends -->
+    {{list.cards}}
+    <!-- footer starts -->
+    <footer class="add-card px-1 py-2">
+      <span>
+        <img src="@/assets/icons/plus-dark.svg" alt="star" class="mr-2" />
+      </span>
+      <span>Add another card</span>
+    </footer>
+    <!-- footer ends -->
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    list: Object
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+div {
+  header {
+    cursor: pointer;
+    span {
+      img {
+        height: 15px;
+      }
+    }
+  }
+  .add-card {
+    cursor: pointer;
+    span {
+      color: grey;
+
+      img {
+        height: 18px;
+      }
+    }
+    &:hover {
+      background: #9e9fa344;
+      border-radius: 5px;
+      span {
+        color: #333;
+      }
+    }
+  }
+}
+</style>
