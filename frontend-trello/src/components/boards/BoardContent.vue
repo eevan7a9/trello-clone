aa5de4da67f219975b5dab265690ce66f26f23f2
eevan7a9 <e7a9@protonmail.com>
2020-02-15 15:12:44 +0900

    added add list button

--- frontend-trello/src/components/boards/BoardContent.vue
+++ frontend-trello/src/components/boards/BoardContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="content-wrapper text-left">
-    <div class="lists-container d-flex px-2">
+    <div class="lists-container d-flex px-2 pr-5">
       <draggable
         class="list-group"
         tag="div"
@@ -15,6 +15,13 @@
           </div>
         </transition-group>
       </draggable>
+      <div class="add-list m-1" @click="showListForm">
+        <div class="open-add-list d-flex align-items-center">
+          <img src="@/assets/icons/plus.svg" alt="plus" />
+          <h6 class="p-0 m-0 text-light">Add another list</h6>
+        </div>
+      </div>
+      <div class="extra-spacer p-2"></div>
     </div>
   </div>
 </template>
@@ -45,7 +52,11 @@ export default {
       };
     }
   },
-  methods: {},
+  methods: {
+    showListForm() {
+      console.log("show the form");
+    }
+  },
   created() {
     this.$store.dispatch("getBoardData", 1);
   }
@@ -83,5 +94,19 @@ export default {
   width: 272px;
   background: transparent;
   border: none;
+}
+.add-list {
+  cursor: pointer;
+  min-width: 280px;
+  background-color: rgba(0, 0, 0, 0.219);
+  border-radius: 3px;
+  height: 42px;
+  &:hover {
+    background: rgba(0, 0, 0, 0.411);
+  }
+  .open-add-list {
+    padding: 20px;
+    height: 32px;
+  }
 }
 </style>
