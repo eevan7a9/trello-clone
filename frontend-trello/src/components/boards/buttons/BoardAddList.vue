8a3f761f588bd5bf232590e1a92683d5566a6d1f
eevan7a9 <e7a9@protonmail.com>
2020-02-16 07:36:24 +0900

    added add list form styles

--- frontend-trello/src/components/boards/buttons/BoardAddList.vue
+++ frontend-trello/src/components/boards/buttons/BoardAddList.vue
@@ -1,21 +1,53 @@
 <template>
-  <div class="add-list m-1" @click="showListForm">
-    <div class="open-add-list d-flex align-items-center">
+  <div class="add-list m-1">
+    <div class="open-add-list d-flex align-items-center" @click="showListForm" v-if="!isFormOpen">
       <img src="@/assets/icons/plus.svg" alt="plus" />
       <h6 class="p-0 m-0 text-light">Add another list</h6>
+    </div>
+    <div class="add-list-form p-1" :class="{'show' : isFormOpen}">
+      <input
+        type="input"
+        class="form-control border-primary"
+        v-model.trim="listTitle"
+        placeholder="Enter list title..."
+        ref="inputList"
+        @keypress.enter="submitForm"
+      />
+      <div class="mt-1">
+        <button class="btn btn-success" @click.stop="submitForm">Add List</button>
+        <span @click.stop="submitForm">
+          <img src="@/assets/icons/x.svg" alt="close" class="ml-1" />
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component, Vue, Prop, Ref } from "vue-property-decorator";
 
 @Component
 export default class BoardAddList extends Vue {
+  @Ref() readonly inputList!: HTMLInputElement;
+
   @Prop(Number) private boardId: number | undefined;
 
+  isFormOpen = false;
+
+  listTitle = "";
+
   showListForm() {
-    console.log("adding list...", this.boardId);
+    this.isFormOpen = true;
+    this.$nextTick(() => {
+      this.inputList.focus();
+    });
+  }
+  submitForm() {
+    if (this.listTitle.length) {
+      console.log("submited");
+      this.listTitle = "";
+    }
+    this.isFormOpen = false;
   }
 }
 </script>
@@ -33,6 +65,20 @@ export default class BoardAddList extends Vue {
   .open-add-list {
     padding: 20px;
     height: 32px;
+  }
+
+  .add-list-form {
+    background: #ebecf0;
+    height: 0;
+    visibility: hidden;
+    transition: height 0.2s ease-out, opacity 0.6s;
+    opacity: 0;
+    &.show {
+      visibility: visible;
+      height: 90px;
+      transition: height 0.2s ease-in, opacity 0.6s;
+      opacity: 1;
+    }
   }
 }
 </style>
