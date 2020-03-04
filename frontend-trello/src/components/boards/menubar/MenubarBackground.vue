dd640d75b9188a672cf4e0c472803433f3402630
eevan7a9 <e7a9@protonmail.com>
2020-03-04 20:00:56 +0900

    added option of bg

--- frontend-trello/src/components/boards/menubar/MenubarBackground.vue
+++ frontend-trello/src/components/boards/menubar/MenubarBackground.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <img src="@/assets/under_construction.svg" class="img-fluid p-2 my-5" alt="constructing" />
-    <h5 class="text-muted text-uppercase font-weight-bold">under construction</h5>
+    <div class="d-flex justify-content-center">
+      <div class="w-100 p-1 pt-3" style="cursor:pointer">
+        <div
+          class="image"
+          style="background-image:url(https://a.trellocdn.com/prgb/dist/images/photos-thumbnail@3x.48948499e309aef794d7.jpg)"
+        ></div>
+        <span class="font-weight-bold">Photos</span>
+      </div>
+      <div class="w-100 p-1 pt-3" style="cursor:pointer">
+        <div
+          class="image"
+          style="background-image:url(https://a.trellocdn.com/prgb/dist/images/colors@2x.864f4df15d825e89e199.jpg)"
+        ></div>
+        <span class="font-weight-bold">Colors</span>
+      </div>
+    </div>
+
+    <!-- <img src="@/assets/under_construction.svg" class="img-fluid p-2 my-5" alt="constructing" />
+    <h5 class="text-muted text-uppercase font-weight-bold">under construction</h5>-->
   </div>
 </template>
 
@@ -12,5 +29,16 @@ import { Component, Vue } from "vue-property-decorator";
 export default class MenubarBackground extends Vue {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.image {
+  background-color: #dfe1e6;
+  background-size: cover;
+  border-radius: 8px;
+  height: 96px;
+  margin-bottom: 8px;
+  width: 100%;
+  &:hover {
+    opacity: 0.8;
+  }
+}
 </style>
